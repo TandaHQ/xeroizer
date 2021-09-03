2c19e03fc89c3ba45ada8c02fc90e4a8c71af011
Philip Fraser <pr.fraser3@gmail.com>
2021-09-03 08:02:42 +0200

    add employee id to payment method model

--- lib/xeroizer/models/payroll/payment_method.rb
+++ lib/xeroizer/models/payroll/payment_method.rb
@@ -12,10 +12,17 @@ module Xeroizer
       end
 
       class PaymentMethod < PayrollBase
-
+        guid            :employee_id
         string          :payment_method
         has_many        :bank_accounts
 
+        def to_api_json
+          JSON.parse(super)
+        end
+
+        def api_url
+          "employees/#{employee_id}/paymentMethods"
+        end
       end
     end
   end
