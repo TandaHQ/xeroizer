606ce1c6080e0874a2d912546a9008d16cbc0e7b
Ben Aitcheson <benaitcheson@hotmail.com>
2022-11-21 16:25:31 +1000

    WIP: add categories

--- lib/xeroizer/models/payroll/leave_type.rb
+++ lib/xeroizer/models/payroll/leave_type.rb
@@ -10,16 +10,17 @@ module Xeroizer
 
         string        :name
         string        :type_of_units
-        boolean       :is_paid_leave
+        boolean       :is_paid_leave # if paid leave this will need :leave_category_code
         boolean       :show_on_payslip
 
         guid          :leave_type_id
         decimal       :normal_entitlement
         decimal       :leave_loading_rate
+        string        :leave_category_code, api_name: 'LeaveCategoryCode' # https://developer.xero.com/documentation/api/payrollau/payitems#elements-for-leavetypes
 
         datetime_utc  :updated_date_utc, :api_name => 'UpdatedDateUTC'
 
-        validates_presence_of :name, :type_of_units, :is_paid_leave, :show_on_payslip
+        validates_presence_of :name, :type_of_units, :is_paid_leave, :show_on_payslip, :leave_category_code
 
       end
 
